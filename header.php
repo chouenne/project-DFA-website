b84527cebecdc059c2e71b60aeb341207950cb51
chouenne <134743005+chouenne@users.noreply.github.com>
2025-03-27 15:53:59 -0400

    adjust the responsiveness issue, the right white spacefor the mobile version and add the space under the services cards

--- header.php
+++ header.php
@@ -29,7 +29,7 @@
         </div>
 
         <!-- Menu Toggle for Mobile -->
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
           aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
